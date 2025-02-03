3747df9278bb94c3bd27f7a0a18a5be2ab50a95f
luismoura03 <gui.batidao3@gmail.com>
2025-02-03 14:23:56 -0300

    :recycle: New modal for add authors

---
+++ src/components/AddDialog/AddAuthorDialog.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-dialog :model-value="isOpen" @update:model-value="closeDialog()" persistent>
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Adicionar Autor</div>
+      </q-card-section>
+      <q-card-section>
+        <div class="row q-gutter-lg">
+          <q-input
+            v-model="localAuthorData.name"
+            label="Nome do Autor"
+            filled
+            style="max-width: 300px;"
+          />
+        </div>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat label="Cancelar" color="primary" @click="closeDialog" />
+        <q-btn flat label="Criar" color="green" @click="saveChanges"  />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  isOpen: Boolean,
+})
+
+const emit = defineEmits(['close', 'save'])
+
+watch(() => props.isOpen, (isOpen) => {
+  if(isOpen) {
+    localAuthorData.value = { name: ''}
+  }
+})
+
+const localAuthorData = ref({
+  name: ''
+})
+
+const closeDialog = () => {
+  emit('close')
+}
+
+const saveChanges = () => {
+  emit('save', localAuthorData.value)
+  localAuthorData.value = { name: ''}
+}
+</script>
