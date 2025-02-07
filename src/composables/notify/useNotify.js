19b09515f917ace3b4ae8a17d65bf04c065db6de
luismoura03 <gui.batidao3@gmail.com>
2025-02-07 10:58:06 -0300

    ♻️ Create composables for notify

---
+++ src/composables/notify/useNotify.js
@@ -0,0 +1,64 @@
+import { useQuasar } from "quasar";
+
+export default function useNotify() {
+  const $q = useQuasar();
+
+  function notifySucess({
+    message,
+    color = "positive",
+    icon = "done",
+    position = "bottom-right",
+  }) {
+    $q.notify({
+      message,
+      color,
+      icon,
+      position,
+    });
+  }
+  function notifyInfo({
+    message,
+    color = "blue",
+    icon = "info",
+    position = "bottom-right",
+  }) {
+    $q.notify({
+      message,
+      color,
+      icon,
+      position,
+    });
+  }
+  function notifyError({
+    message,
+    color = "blue",
+    icon = "info",
+    position = "bottom-right",
+  }) {
+    $q.notify({
+      message,
+      color,
+      icon,
+      position,
+    });
+  }
+  function notifyWarning({
+    message,
+    color = "warning",
+    icon = "warning",
+    position = "bottom-right",
+  }) {
+    $q.notify({
+      message,
+      color,
+      icon,
+      position,
+    });
+  }
+  return {
+    notifySucess,
+    notifyInfo,
+    notifyError,
+    notifyWarning,
+  };
+}
